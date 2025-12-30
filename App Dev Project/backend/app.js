55c55ef6a56e3374307017dc7ea493812263134b
Janhavi Chaudhari <janhavich99@gmail.com>
2025-12-30 12:45:27 +0530

    changes

--- App Dev Project/backend/app.js
+++ App Dev Project/backend/app.js
@@ -5,4 +5,5 @@ const app = express()
 app.get('/',(req,res)=>{
     res.send("hello world")
 })
+
 module.exports = app
--- App Dev Project/backend/model/todo.model.js
+++ App Dev Project/backend/model/todo.model.js
@@ -2,6 +2,10 @@ const monogdb = require('../config/monogdb')
 const mongoose = require('mongoose')
 
 const toDoSchema = new mongoose.Schema({
+    id:{
+        type: number,
+        required: true
+    },
     taskName: {
         type: String,
         required: true
