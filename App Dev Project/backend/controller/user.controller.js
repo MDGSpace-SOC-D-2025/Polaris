6c026a87d4402597b16047f41ac83736da9421cf
Janhavi Chaudhari <janhavich99@gmail.com>
2026-01-03 19:11:27 +0530

    jwt

--- App Dev Project/backend/controller/user.controller.js
+++ App Dev Project/backend/controller/user.controller.js
@@ -1,5 +1,6 @@
 const userModel = require("../model/user.model")
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const getUser = async (req, res) => {
   const user = await userModel.find()
@@ -10,7 +11,15 @@ const addUser = async (req, res) => {
   const {email, userId, password} = req.body;
   const hashPassword = await bcrypt.hash(password, 10);
   await userModel.create({email, userId, password: hashPassword})
-  res.json({ message: "User added" })
+  const token = jwt.sign({
+      userId: req.body.userId
+    },
+  process.env.JWT_SECRET
+  )
+  res.cookie('token',token)
+  res.json({ 
+    message: "User added",
+  })
 };
 
 module.exports = {
